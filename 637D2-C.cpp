9fb2aa3a8458f020ac7a2f76a9527831ebf3b4c1
Yuxun Lu <lu-yuxun@nii.ac.jp>
2020-05-01 09:51:41 +0900

    before modifying method.

--- 637D2-C.cpp
+++ 637D2-C.cpp
@@ -12,56 +12,73 @@ bool solve(std::vector<int> current_p, int target_num, std::vector<int> target_p
     bool res = false;
     // int p_[MAX_SIZE] = {0};
     // std::memcpy(p_, p, sizeof(int) * MAX_SIZE);
-    std::vector<int> cnt(n+1);
-    std::vector<int> r(n+1);
-
     // compute r from current_p
+
+    // initialize stage.
+    std::vector<int> r(n+1);
+    std::vector<std::vector<int>> aux_vec(n+1);
+    std::vector<int> cnt(n+1);
     for(int i = 1; i <= n; i++)
     {
-        bool flag = true;
-        for(int j=i; j<=n; j++)
+        r[i] = i;
+        aux_vec[i].push_back(i);
+        cnt[i] = 1;
+    }
+    
+    for(int nmsl=0; nmsl < n; nmsl++)
+    {
+        // compute cnt
+        /* for(int t=1; t<=n; t++)
         {
-            if (current_p[j] == 0)
+            cnt[t] = std::count(r.begin(), r.end(), t);
+        } */
+        // target location
+        // std::vector::iterator
+        auto target_ind = std::find(target_p.begin(), target_p.end(), target_num);
+        int t_ind = std::distance(target_p.begin(), target_ind);
+        int max_count = *std::max_element(cnt.begin(), cnt.end());
+        // judge if cnt[target_ind] == max_count.
+        // passed -> fill in, go to next round.
+        // otherwise -> return false;
+        if (cnt[t_ind] == max_count)
+        {
+            current_p[t_ind] = target_num;
+            // update r
+            if (t_ind + 1 <= n)
             {
-                flag = true;
-                r[i] = j;
+                r[t_ind] = r[t_ind + 1];
+                aux_vec[t_ind+1].push_back(t_ind);
+            }
+            else
+                r[t_ind] = -1;
+            cnt[t_ind] = 0; //since it's taken.
+            while(!aux_vec[t_ind].empty())
+            {
+                int ptr = aux_vec[t_ind].front();
+                r[ptr] = r[t_ind];
+                if (r[ptr] != -1)
+                {
+                    aux_vec[r[ptr]].push_back(ptr);
+                    cnt[r[ptr]] = cnt[r[ptr]] + 1;
+                }
+                aux_vec[t_ind].pop_back();
+            }
+            // update r ends.
+            if (target_num < n)
+                target_num = target_num + 1;
+            else
+            {
+                res = true;
                 break;
             }
         }
-        if (!flag)
-            r[i] = -1; //indefine.
-    }
-    for(int t=1; t<=n; t++)
-    {
-        cnt[t] = std::count(r.begin(), r.end(), t);
-    }
-    // target location
-    // std::vector::iterator
-    auto target_ind = std::find(target_p.begin(), target_p.end(), target_num);
-    int t_ind = std::distance(target_p.begin(), target_ind);
-    int max_count = *std::max_element(cnt.begin(), cnt.end());
-    // judge if cnt[target_ind] == max_count.
-    // passed -> fill in, go to next round.
-    // otherwise -> return false;
-    if (cnt[t_ind] == max_count)
-    {
-        current_p[t_ind] = target_num;
-        if (target_num < n)
-        {
-            target_num = target_num + 1;
-            res = solve(current_p, target_num, target_p, n);
-        }
         else
         {
-            res = true;
+            res = false;
+            break;
         }
-        
     }
-    else
-    {
-        res = false;
-    }
-    return res; 
+    return res;
 }
 int main(int argc, char* argv[])
 {
